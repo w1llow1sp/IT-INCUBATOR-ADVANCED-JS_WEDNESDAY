c9821548b878d6e1bb8896d2b1f7c754d010bc1c
w1llow1sp <mari.front.74@gmail.com>
2023-06-06 18:50:40 +0500

    Solved wednesday third week ( ready only simple search)

--- src/lessons/lesson3/API.ts
+++ src/lessons/lesson3/API.ts
@@ -3,13 +3,38 @@ import axios from 'axios';
 const configOMB = {
     baseURL: 'http://www.omdbapi.com',
 };
-const key = '';
+const key =  'f5284b56';
 const axiosInstance = axios.create(configOMB);
 
 const API = {
     searchFilmsByTitle: (title: string) => {
+        return axiosInstance
+            .get('/', {
+                params: {
+                    apikey: key,
+                    s: title,
+                },
+            })
+            .then((response) => response.data)
+            .catch((error) => {
+                console.log('Can\'t search films by title', error);
+                throw error;
+            });
     },
     searchFilmsByType: (title: string, type: string) => {
+        return axiosInstance
+            .get('/', {
+                params: {
+                    apikey: key,
+                    s: title,
+                    type: type,
+                },
+            })
+            .then((response) => response.data)
+            .catch((error) => {
+                console.log('Can\'t search films by type', error);
+                throw error;
+            });
     }
 };
 
--- src/lessons/lesson3/Lesson3.tsx
+++ src/lessons/lesson3/Lesson3.tsx
@@ -2,14 +2,30 @@ import React, { useState } from 'react';
 import API from './API';
 import './lesson_3';
 
+type serachResultType = {
+
+    Title: string
+    Year: string
+    imdbID: string
+    Type: 'movie'| 'series'| 'episode'
+    Poster: string
+
+}
+
+
 const Lesson3 = () => {
     const [searchName, setSearchName] = useState('');
-    const [serachResult, setSerachResult] = useState('');
+    const [serachResult, setSerachResult] = useState<Array<serachResultType>>([]);
     const [searchNameByType, setSearchNameByType] = useState('');
     const [serachResultByType, setSerachResultByType] = useState('');
 
+
     const searchFilm = () => {
         API.searchFilmsByTitle(searchName)
+            .then((data) => {
+                setSerachResult(data.Search);
+                console.log(data.Search)
+            })
     };
 
     const searchByType = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -24,8 +40,18 @@ const Lesson3 = () => {
                 <h3><p>Search by name:</p></h3>
                 <input type="text" value={searchName} onChange={(e) => setSearchName(e.currentTarget.value)}/>
                 <button onClick={searchFilm}>Search</button>
-                <div>
-                    {serachResult}
+                <div className={'search_container'}>
+                    {Array.isArray(serachResult) && serachResult.map((el) =>
+                        (
+
+                            <div key={el.imdbID} className={'search_card'}>
+                                <h3 className={'search_header'}>Title: {el.Title}</h3>
+                                <p className={'search_year'}>Year : {el.Year}</p>
+                                <p className={'search_type'}>Type: {el.Type}</p>
+                                <img src={el.Poster} className={'search_img'}/>
+                            </div>
+
+                        ))}
                 </div>
             </div>
 
